997276eaea47899812531cf0720e9567f7b9a7e3
Alessandro Molina <alessandro@molina.fyi>
2026-01-20 20:28:33 +0100

    Remove Python 3-only classifier

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
                     'License :: OSI Approved :: BSD License',
                     'Programming Language :: C',
                     'Programming Language :: Python :: 3',
-                    'Programming Language :: Python :: 3 :: Only',
                     'Programming Language :: Python :: 3.8',
                     'Programming Language :: Python :: 3.9',
                     'Programming Language :: Python :: 3.10',
